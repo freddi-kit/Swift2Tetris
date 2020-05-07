beddd6f114251d5e95f341fdce4484df2751c390
freddi <bohemianyuki@gmail.com>
2020-05-08 00:10:04 +0900

    Organized Package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftdeOS",
+    products: [
+        .executable(name: "SwiftdeOS", targets: ["SwiftdeOS"]),
+        .library(name: "SwiftdeOSKit", targets: ["SwiftdeOSKit"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -14,9 +18,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftdeOS",
+            dependencies: ["SwiftdeOSKit"]),
+        .target(
+            name: "SwiftdeOSKit",
             dependencies: []),
         .testTarget(
             name: "SwiftdeOSTests",
-            dependencies: ["SwiftdeOS"]),
+            dependencies: ["SwiftdeOSKit"]),
     ]
 )
