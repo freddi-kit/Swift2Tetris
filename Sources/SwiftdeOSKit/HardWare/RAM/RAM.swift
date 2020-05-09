5bcccf5c9721f5f65c1dbb4a1f7c5d4094373077
freddi <bohemianyuki@gmail.com>
2020-05-10 02:21:42 +0900

    Implemented all RAM

--- Sources/SwiftdeOSKit/HardWare/RAM/RAM.swift
+++ Sources/SwiftdeOSKit/HardWare/RAM/RAM.swift
@@ -99,7 +99,7 @@ class RAM64 {
 
 class RAM512 {
 
-    private var reg = (
+    private var ram64 = (
         RAM64(),
         RAM64(),
         RAM64(),
@@ -109,11 +109,32 @@ class RAM512 {
         RAM64(),
         RAM64()
     )
+    
+    func out(in: Bit16, load: Bit, address: (Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit)) -> Bit16 {
+        let loadReg = Plexor.deMultiPlexor8way(in: load,
+                                               sel2: address.0,
+                                               sel1: address.1,
+                                               sel0: address.2)
+        
+        
+        let address = (address.3, address.4, address.5, address.6, address.7, address.8)
+        return Plexor.multiPlexor8way(a: ram64.0.out(in: `in`, load: loadReg.a, address: address),
+                                      b: ram64.1.out(in: `in`, load: loadReg.b, address: address),
+                                      c: ram64.2.out(in: `in`, load: loadReg.c, address: address),
+                                      d: ram64.3.out(in: `in`, load: loadReg.d, address: address),
+                                      e: ram64.4.out(in: `in`, load: loadReg.e, address: address),
+                                      f: ram64.5.out(in: `in`, load: loadReg.f, address: address),
+                                      g: ram64.6.out(in: `in`, load: loadReg.g, address: address),
+                                      h: ram64.7.out(in: `in`, load: loadReg.h, address: address),
+                                      sel2: address.0,
+                                      sel1: address.1,
+                                      sel0: address.2)
+    }
 }
 
 class RAM4096 {
 
-    private var reg = (
+    private var ram512 = (
         RAM512(),
         RAM512(),
         RAM512(),
@@ -123,11 +144,33 @@ class RAM4096 {
         RAM512(),
         RAM512()
     )
+    
+    func out(in: Bit16, load: Bit, address: (Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit)) -> Bit16 {
+        let loadReg = Plexor.deMultiPlexor8way(in: load,
+                                               sel2: address.0,
+                                               sel1: address.1,
+                                               sel0: address.2)
+        
+        
+        let address = (address.3, address.4, address.5, address.6, address.7, address.8, address.9, address.10, address.11)
+        
+        return Plexor.multiPlexor8way(a: ram512.0.out(in: `in`, load: loadReg.a, address: address),
+                                      b: ram512.1.out(in: `in`, load: loadReg.b, address: address),
+                                      c: ram512.2.out(in: `in`, load: loadReg.c, address: address),
+                                      d: ram512.3.out(in: `in`, load: loadReg.d, address: address),
+                                      e: ram512.4.out(in: `in`, load: loadReg.e, address: address),
+                                      f: ram512.5.out(in: `in`, load: loadReg.f, address: address),
+                                      g: ram512.6.out(in: `in`, load: loadReg.g, address: address),
+                                      h: ram512.7.out(in: `in`, load: loadReg.h, address: address),
+                                      sel2: address.0,
+                                      sel1: address.1,
+                                      sel0: address.2)
+    }
 }
 
 class RAM16384 {
 
-    private var reg = (
+    private var ram4096 = (
         RAM4096(),
         RAM4096(),
         RAM4096(),
@@ -137,12 +180,34 @@ class RAM16384 {
         RAM4096(),
         RAM4096()
     )
+    
+    func out(in: Bit16, load: Bit, address: (Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit)) -> Bit16 {
+        let loadReg = Plexor.deMultiPlexor8way(in: load,
+                                               sel2: address.0,
+                                               sel1: address.1,
+                                               sel0: address.2)
+        
+        
+        let address = (address.3, address.4, address.5, address.6, address.7, address.8, address.9, address.10, address.11, address.12, address.13, address.14)
+        
+        return Plexor.multiPlexor8way(a: ram4096.0.out(in: `in`, load: loadReg.a, address: address),
+                                      b: ram4096.1.out(in: `in`, load: loadReg.b, address: address),
+                                      c: ram4096.2.out(in: `in`, load: loadReg.c, address: address),
+                                      d: ram4096.3.out(in: `in`, load: loadReg.d, address: address),
+                                      e: ram4096.4.out(in: `in`, load: loadReg.e, address: address),
+                                      f: ram4096.5.out(in: `in`, load: loadReg.f, address: address),
+                                      g: ram4096.6.out(in: `in`, load: loadReg.g, address: address),
+                                      h: ram4096.7.out(in: `in`, load: loadReg.h, address: address),
+                                      sel2: address.0,
+                                      sel1: address.1,
+                                      sel0: address.2)
+    }
 }
 
 
 class RAM32k {
 
-    private var reg = (
+    private var ram16k = (
         RAM16384(),
         RAM16384(),
         RAM16384(),
@@ -152,5 +217,27 @@ class RAM32k {
         RAM16384(),
         RAM16384()
     )
+    
+    func out(in: Bit16, load: Bit, address: (Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit)) -> Bit16 {
+        let loadReg = Plexor.deMultiPlexor8way(in: load,
+                                               sel2: address.0,
+                                               sel1: address.1,
+                                               sel0: address.2)
+        
+        
+        let address = (address.3, address.4, address.5, address.6, address.7, address.8, address.9, address.10, address.11, address.12, address.13, address.14, address.15, address.16, address.17)
+        
+        return Plexor.multiPlexor8way(a: ram16k.0.out(in: `in`, load: loadReg.a, address: address),
+                                      b: ram16k.1.out(in: `in`, load: loadReg.b, address: address),
+                                      c: ram16k.2.out(in: `in`, load: loadReg.c, address: address),
+                                      d: ram16k.3.out(in: `in`, load: loadReg.d, address: address),
+                                      e: ram16k.4.out(in: `in`, load: loadReg.e, address: address),
+                                      f: ram16k.5.out(in: `in`, load: loadReg.f, address: address),
+                                      g: ram16k.6.out(in: `in`, load: loadReg.g, address: address),
+                                      h: ram16k.7.out(in: `in`, load: loadReg.h, address: address),
+                                      sel2: address.0,
+                                      sel1: address.1,
+                                      sel0: address.2)
+    }
 }
 
