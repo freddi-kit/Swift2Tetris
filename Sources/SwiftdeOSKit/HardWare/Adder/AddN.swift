4168367cc76bcb41027006de9a9e29cffe013ee6
freddi <bohemianyuki@gmail.com>
2020-05-09 23:13:05 +0900

    Added Bit15

--- Sources/SwiftdeOSKit/HardWare/Adder/AddN.swift
+++ Sources/SwiftdeOSKit/HardWare/Adder/AddN.swift
@@ -9,6 +9,40 @@ import Foundation
 
 // TODO: make as template
 extension Adder {
+    static func Add15(a: Bit15, b: Bit15) -> Bit15 {
+        let b14 = halfAdder(a: a.bits.14, b: b.bits.14)
+        let b13 = fullAdder(a: a.bits.13, b: b.bits.13, c: b14.carry)
+        let b12 = fullAdder(a: a.bits.12, b: b.bits.12, c: b13.carry)
+        let b11 = fullAdder(a: a.bits.11, b: b.bits.11, c: b12.carry)
+        let b10 = fullAdder(a: a.bits.10, b: b.bits.10, c: b11.carry)
+        let b9 = fullAdder(a: a.bits.9, b: b.bits.9, c: b10.carry)
+        let b8 = fullAdder(a: a.bits.8, b: b.bits.8, c: b9.carry)
+        let b7 = fullAdder(a: a.bits.7, b: b.bits.7, c: b8.carry)
+        let b6 = fullAdder(a: a.bits.6, b: b.bits.6, c: b7.carry)
+        let b5 = fullAdder(a: a.bits.5, b: b.bits.5, c: b6.carry)
+        let b4 = fullAdder(a: a.bits.4, b: b.bits.4, c: b5.carry)
+        let b3 = fullAdder(a: a.bits.3, b: b.bits.3, c: b4.carry)
+        let b2 = fullAdder(a: a.bits.2, b: b.bits.2, c: b3.carry)
+        let b1 = fullAdder(a: a.bits.1, b: b.bits.1, c: b2.carry)
+        let b0 = fullAdder(a: a.bits.0, b: b.bits.0, c: b1.carry)
+        return Bit15(bits: (
+            b0.sum,
+            b1.sum,
+            b2.sum,
+            b3.sum,
+            b4.sum,
+            b5.sum,
+            b6.sum,
+            b7.sum,
+            b8.sum,
+            b9.sum,
+            b10.sum,
+            b11.sum,
+            b12.sum,
+            b13.sum,
+            b14.sum
+        ))
+    }
     
     static func Add16(a: Bit16, b: Bit16) -> Bit16 {
         let b15 = halfAdder(a: a.bits.15, b: b.bits.15)
--- Sources/SwiftdeOSKit/HardWare/Bit/BitN.swift
+++ Sources/SwiftdeOSKit/HardWare/Bit/BitN.swift
@@ -8,6 +8,140 @@
 import Foundation
 
 // TODO: make as template
+struct Bit15: BitCalculatable {
+    
+    static let allLow: Bit15 = Bit15(bits: (.low, .low, .low, .low, .low, .low, .low, .low, .low, .low, .low, .low, .low, .low, .low))
+    static let allHigh: Bit15 = Bit15(bits: (.high, .high, .high, .high, .high, .high, .high, .high, .high, .high, .high, .high, .high, .high, .high))
+    
+    var bits: (Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit)
+    
+    static func nand(x: Bit15, y: Bit15) -> Bit15 {
+        Bit15(bits:(
+            Bit.nand(x: x.bits.0, y: y.bits.0),
+            Bit.nand(x: x.bits.1, y: y.bits.1),
+            Bit.nand(x: x.bits.2, y: y.bits.2),
+            Bit.nand(x: x.bits.3, y: y.bits.3),
+            Bit.nand(x: x.bits.4, y: y.bits.4),
+            Bit.nand(x: x.bits.5, y: y.bits.5),
+            Bit.nand(x: x.bits.6, y: y.bits.6),
+            Bit.nand(x: x.bits.7, y: y.bits.7),
+            Bit.nand(x: x.bits.8, y: y.bits.8),
+            Bit.nand(x: x.bits.9, y: y.bits.9),
+            Bit.nand(x: x.bits.10, y: y.bits.10),
+            Bit.nand(x: x.bits.11, y: y.bits.11),
+            Bit.nand(x: x.bits.12, y: y.bits.12),
+            Bit.nand(x: x.bits.13, y: y.bits.13),
+            Bit.nand(x: x.bits.14, y: y.bits.14)
+            )
+        )
+    }
+    
+    static func or(x: Bit15, y: Bit15) -> Bit15 {
+        Bit15(bits:(
+            Bit.or(x: x.bits.0, y: y.bits.0),
+            Bit.or(x: x.bits.1, y: y.bits.1),
+            Bit.or(x: x.bits.2, y: y.bits.2),
+            Bit.or(x: x.bits.3, y: y.bits.3),
+            Bit.or(x: x.bits.4, y: y.bits.4),
+            Bit.or(x: x.bits.5, y: y.bits.5),
+            Bit.or(x: x.bits.6, y: y.bits.6),
+            Bit.or(x: x.bits.7, y: y.bits.7),
+            Bit.or(x: x.bits.8, y: y.bits.8),
+            Bit.or(x: x.bits.9, y: y.bits.9),
+            Bit.or(x: x.bits.10, y: y.bits.10),
+            Bit.or(x: x.bits.11, y: y.bits.11),
+            Bit.or(x: x.bits.12, y: y.bits.12),
+            Bit.or(x: x.bits.13, y: y.bits.13),
+            Bit.or(x: x.bits.14, y: y.bits.14)
+            )
+        )
+    }
+    
+    static func and(x: Bit15, y: Bit15) -> Bit15 {
+        Bit15(bits:(
+            Bit.and(x: x.bits.0, y: y.bits.0),
+            Bit.and(x: x.bits.1, y: y.bits.1),
+            Bit.and(x: x.bits.2, y: y.bits.2),
+            Bit.and(x: x.bits.3, y: y.bits.3),
+            Bit.and(x: x.bits.4, y: y.bits.4),
+            Bit.and(x: x.bits.5, y: y.bits.5),
+            Bit.and(x: x.bits.6, y: y.bits.6),
+            Bit.and(x: x.bits.7, y: y.bits.7),
+            Bit.and(x: x.bits.8, y: y.bits.8),
+            Bit.and(x: x.bits.9, y: y.bits.9),
+            Bit.and(x: x.bits.10, y: y.bits.10),
+            Bit.and(x: x.bits.11, y: y.bits.11),
+            Bit.and(x: x.bits.12, y: y.bits.12),
+            Bit.and(x: x.bits.13, y: y.bits.13),
+            Bit.and(x: x.bits.14, y: y.bits.14)
+            )
+        )
+    }
+    
+    static func not(x: Bit15) -> Bit15 {
+        Bit15(bits:(
+            Bit.not(x: x.bits.0),
+            Bit.not(x: x.bits.1),
+            Bit.not(x: x.bits.2),
+            Bit.not(x: x.bits.3),
+            Bit.not(x: x.bits.4),
+            Bit.not(x: x.bits.5),
+            Bit.not(x: x.bits.6),
+            Bit.not(x: x.bits.7),
+            Bit.not(x: x.bits.8),
+            Bit.not(x: x.bits.9),
+            Bit.not(x: x.bits.10),
+            Bit.not(x: x.bits.11),
+            Bit.not(x: x.bits.12),
+            Bit.not(x: x.bits.13),
+            Bit.not(x: x.bits.14)
+            )
+        )
+    }
+    
+    static func xor(x: Bit15, y: Bit15) -> Bit15 {
+        Bit15(bits:(
+            Bit.xor(x: x.bits.0, y: y.bits.0),
+            Bit.xor(x: x.bits.1, y: y.bits.1),
+            Bit.xor(x: x.bits.2, y: y.bits.2),
+            Bit.xor(x: x.bits.3, y: y.bits.3),
+            Bit.xor(x: x.bits.4, y: y.bits.4),
+            Bit.xor(x: x.bits.5, y: y.bits.5),
+            Bit.xor(x: x.bits.6, y: y.bits.6),
+            Bit.xor(x: x.bits.7, y: y.bits.7),
+            Bit.xor(x: x.bits.8, y: y.bits.8),
+            Bit.xor(x: x.bits.9, y: y.bits.9),
+            Bit.xor(x: x.bits.10, y: y.bits.10),
+            Bit.xor(x: x.bits.11, y: y.bits.11),
+            Bit.xor(x: x.bits.12, y: y.bits.12),
+            Bit.xor(x: x.bits.13, y: y.bits.13),
+            Bit.xor(x: x.bits.14, y: y.bits.14)
+            )
+        )
+    }
+    
+    static func incrementor(x: Bit15) -> Bit15 {
+         Adder.Add15(a: x,
+                     b: Bit15(bits: (
+                         .low,
+                         .low,
+                         .low,
+                         .low,
+                         .low,
+                         .low,
+                         .low,
+                         .low,
+                         .low,
+                         .low,
+                         .low,
+                         .low,
+                         .low,
+                         .low,
+                         .high)))
+     }
+}
+
+
 struct Bit16: BitCalculatable {
     
     static let allLow: Bit16 = Bit16(bits: (.low, .low, .low, .low, .low, .low, .low, .low, .low, .low, .low, .low, .low, .low, .low, .low))
