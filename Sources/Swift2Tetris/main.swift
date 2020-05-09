b8489163a11b02bc31a7c3628ee5bbd3b23a2164
freddi <bohemianyuki@gmail.com>
2020-05-10 05:07:25 +0900

    Fixed main

--- Sources/Swift2Tetris/main.swift
+++ Sources/Swift2Tetris/main.swift
@@ -1,10 +1,1 @@
-import Swift2TetrisKit
-
-var computer = Computer(instructionRAM: RAM32k())
-computer.oneClock(reset: .low)
-computer.oneClock(reset: .low)
-computer.oneClock(reset: .low)
-computer.oneClock(reset: .low)
-computer.oneClock(reset: .low)
-computer.oneClock(reset: .low)
-computer.oneClock(reset: .low)
+print("Hello, world!")
